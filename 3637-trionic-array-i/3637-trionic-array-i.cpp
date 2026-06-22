b8ce8dc29723d6fb5f169f527a2d8a0cc8186a50
Avijit Ghosh <93346632+AviG10@users.noreply.github.com>
2026-06-23 02:00:22 +0530

    Time: 0 ms (100%), Space: 27.7 MB (74.25%) - LeetHub

---
+++ 3637-trionic-array-i/3637-trionic-array-i.cpp
@@ -0,0 +1,24 @@
+// Clean solution
+class Solution {
+public:
+    bool isTrionic(vector<int>& nums) {
+        int i = 0; // stsrting Index
+        int n = nums.size();
+
+        while(i+1 < n && nums[i+1] > nums[i]) //Increasing
+            i++;
+
+        if(i == 0  || i == n-1) return false;
+
+        while(i+1 < n && nums[i+1] < nums[i]) //Decreasing
+            i++;
+
+        if(i == n-1) return false;
+
+        while(i+1 < n && nums[i+1] > nums[i]) //Increasing
+            i++;
+
+        if(i == n-1) return true;
+        else return false;
+    }
+};
