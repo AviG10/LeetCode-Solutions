c173e30150077126ec3e7814ff22eaf6b5b9b16b
Avijit Ghosh <93346632+AviG10@users.noreply.github.com>
2026-06-23 01:01:19 +0530

    Time: 35 ms (50.26%), Space: 80.2 MB (89.49%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -1,30 +1,20 @@
-// Counting Sorting + Greedy
-// TC - O(n)
-// SC - O(n)
+// Sorting + Greedy
+// TC - O(n logn)
+// SC - O(1)
 class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) {
-        int maxCost = 0;
-        for(int &cost : costs){
-            maxCost = max(maxCost, cost);
-        }
-
-        vector<int> countCoins(maxCost+1, 0);
-        for(int &cost : costs){
-            countCoins[cost]++;
-        }
+        sort(begin(costs), end(costs));
         
         int maxIceBar = 0;
         int totalCost = 0;
         
-        for(int i = 0;i < countCoins.size(); i++){
-            if(totalCost >= coins) break;
-
-            if(countCoins[i] != 0){
-               int coinCanbeTaken = min( countCoins[i] , (coins - totalCost) / i);
-               maxIceBar += coinCanbeTaken;
-               totalCost += coinCanbeTaken * i;
+        for(int &cost : costs){
+            if(totalCost + cost <= coins){
+                totalCost += cost;
+                maxIceBar++;
             }
+            else break;
         }
 
         return maxIceBar;
