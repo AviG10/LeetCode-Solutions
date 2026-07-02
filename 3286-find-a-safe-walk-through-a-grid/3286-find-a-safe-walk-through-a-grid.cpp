fe08503a6403f28fb36683d45994d03fb172439d
Avijit Ghosh <93346632+AviG10@users.noreply.github.com>
2026-07-03 01:36:35 +0530

    Time: 7 ms (92.46%), Space: 32.8 MB (43.74%) - LeetHub

--- 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
@@ -1,52 +1,104 @@
-// DFS + Memoization
-// TC: O(m * n * health)
+// 0 - 1 BFS ( Optimized Approach )
+// TC: O(m * n)
 // SC: O(m * n)
 class Solution {
 private:
-    bool isValid(int i, int j, int& m, int& n) {
-        return (i >= 0 && i < m && j >= 0 && j < n);
+    bool isValid(int &x, int &y, int &m, int &n){
+        return (x >= 0 && x < m && y >= 0 && y < n);
     }
-
-    bool solve(int i, int j, int health, int& m, int& n, vector<vector<int>>& grid, vector<vector<bool>>& vis, vector<vector<vector<int>>>& dp) {
-        if(health < 1) return false;
-
-        if (i == m - 1 && j == n - 1) {
-            if (health >= 1)
-                return true;
-            else
-                return false;
-        }
-
-        if (dp[i][j][health] != -1)
-            return dp[i][j][health];
-
-        int dirX[4] = {1, 0, -1, 0};
-        int dirY[4] = {0, 1, 0, -1};
-
-        vis[i][j] = true;
-
-        bool res = false;
-
-        for (int idx = 0; idx < 4; idx++) {
-            int newX = i + dirX[idx];
-            int newY = j + dirY[idx];
-
-            if (isValid(newX, newY, m, n) && !vis[newX][newY]) {
-                res = res || solve(newX, newY, health - grid[newX][newY], m, n, grid, vis, dp);
-            }
-        }
-
-        vis[i][j] = false;
-
-        return dp[i][j][health] = res;
-    }
-
 public:
     bool findSafeWalk(vector<vector<int>>& grid, int health) {
         int m = grid.size(), n = grid[0].size();
-        vector<vector<bool>> vis(m, vector<bool>(n, false));
-        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(health + 1, -1)));
 
-        return solve(0, 0, health - grid[0][0], m, n, grid, vis, dp);
+        vector<vector<int>> minimumHealhRequired(m, vector<int>(n, INT_MAX));
+        deque<pair<int, int>> dq;
+
+        minimumHealhRequired[0][0] = grid[0][0];
+        dq.push_front({0, 0});
+
+        while (!dq.empty()) {
+            auto it = dq.front();
+            dq.pop_front();
+
+            int x = it.first;
+            int y = it.second;
+
+            int dirX[4] = {1, 0, -1, 0};
+            int dirY[4] = {0, 1, 0, -1};
+
+            for (int i = 0; i < 4; i++) {
+                int newX = x + dirX[i];
+                int newY = y + dirY[i];
+
+                if(isValid(newX, newY, m, n)){
+                    if(minimumHealhRequired[x][y] + grid[newX][newY] < minimumHealhRequired[newX][newY]){
+                        minimumHealhRequired[newX][newY] = minimumHealhRequired[x][y] + grid[newX][newY];
+
+                        if(grid[newX][newY] == 0)
+                            dq.push_front({newX, newY});
+                        else 
+                            dq.push_back({newX, newY});
+                    }
+                }
+            }
+        }
+
+        return ( health >= minimumHealhRequired[m-1][n-1] + 1 );
     }
 };
+
+// DFS + Memoization
+// TC: O(m * n * health)
+// SC: O(m * n)
+// class Solution {
+// private:
+//     bool isValid(int i, int j, int& m, int& n) {
+//         return (i >= 0 && i < m && j >= 0 && j < n);
+//     }
+
+//     bool solve(int i, int j, int health, int& m, int& n, vector<vector<int>>&
+//     grid, vector<vector<bool>>& vis, vector<vector<vector<int>>>& dp) {
+//         if(health < 1) return false;
+
+//         if (i == m - 1 && j == n - 1) {
+//             if (health >= 1)
+//                 return true;
+//             else
+//                 return false;
+//         }
+
+//         if (dp[i][j][health] != -1)
+//             return dp[i][j][health];
+
+//         int dirX[4] = {1, 0, -1, 0};
+//         int dirY[4] = {0, 1, 0, -1};
+
+//         vis[i][j] = true;
+
+//         bool res = false;
+
+//         for (int idx = 0; idx < 4; idx++) {
+//             int newX = i + dirX[idx];
+//             int newY = j + dirY[idx];
+
+//             if (isValid(newX, newY, m, n) && !vis[newX][newY]) {
+//                 res = res || solve(newX, newY, health - grid[newX][newY], m,
+//                 n, grid, vis, dp);
+//             }
+//         }
+
+//         vis[i][j] = false;
+
+//         return dp[i][j][health] = res;
+//     }
+
+// public:
+//     bool findSafeWalk(vector<vector<int>>& grid, int health) {
+//         int m = grid.size(), n = grid[0].size();
+//         vector<vector<bool>> vis(m, vector<bool>(n, false));
+//         vector<vector<vector<int>>> dp(m, vector<vector<int>>(n,
+//         vector<int>(health + 1, -1)));
+
+//         return solve(0, 0, health - grid[0][0], m, n, grid, vis, dp);
+//     }
+// };
