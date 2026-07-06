6fbcbf5595109baaed2c8cbb74fc2579f1923a63
Avijit Ghosh <93346632+AviG10@users.noreply.github.com>
2026-07-06 10:54:55 +0530

    Time: 0 ms (100%), Space: 15.2 MB (60.13%) - LeetHub

---
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+
+        auto lambda = [](vector<int>& vec1, vector<int>& vec2) {
+            if (vec1[0] == vec2[0])
+                return vec1[1] > vec2[1];
+
+            else
+                return vec1[0] < vec2[0];
+        };
+
+        sort(intervals.begin(), intervals.end(), lambda);
+
+        int lastIntervalUpperBound = intervals[0][1];
+        int count = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (lastIntervalUpperBound >= intervals[i][1])
+                continue;
+
+            lastIntervalUpperBound = intervals[i][1];
+            count++;
+        }
+
+        return count;
+    }
+};
