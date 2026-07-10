a6a0265f5332a86829557a76b23ce602d10d097b
Avijit Ghosh <93346632+AviG10@users.noreply.github.com>
2026-07-11 01:47:09 +0530

    Time: 8 ms (67.34%), Space: 223.9 MB (86.92%) - LeetHub

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
@@ -1,49 +1,15 @@
-// Disjoint Set Union
-class DisjointSetUnion{
-private:
-    vector<int> parent, size;
-public:
-    DisjointSetUnion(int n){
-        size.resize(n,0);
-        parent.resize(n);
-
-        for(int i = 0;i < n;i++){
-            parent[i] = i;
-        }
-    }
-    
-    int findParent(int node){
-        if(parent[node] == node) return node;
-        else return parent[node] = findParent(parent[node]);
-    }
-
-    void unionBySize(int u, int v){
-        int parent_u = findParent(u);
-        int parent_v = findParent(v);
-
-        if(parent_u == parent_v) return;
-
-        if(size[parent_u] < size[parent_v]){
-            parent[parent_u] = parent_v;
-            size[parent_v] += size[parent_u];
-        }
-        else{
-            parent[parent_v] = parent_u;
-            size[parent_u] += size[parent_v];
-        }
-    }
-};
-
+// Approach - 2 (Optimal)
 class Solution {
 public:
     vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
 
-        DisjointSetUnion DSU(n);
+        vector<int> component(n, 0);
 
         for(int i = 1;i < n;i++){
-            if(nums[i] - nums[i-1] <= maxDiff){
-                DSU.unionBySize(i, i-1);
-            }
+            if(nums[i] - nums[i-1] <= maxDiff)
+                component[i] = component[i-1];
+            else
+                component[i] = component[i-1] + 1;
         }
 
         int q_size = queries.size();
@@ -53,10 +19,76 @@ public:
             int u = queries[i][0];
             int v = queries[i][1];
 
-            if(DSU.findParent(u) == DSU.findParent(v))
+            if(component[u] == component[v])
                 ans[i] = true;
         }
 
         return ans;
     }
 };
+
+
+
+// Approach - 1 (Better) (Disjoint Set Union)
+
+// class DisjointSetUnion{
+// private:
+//     vector<int> parent, size;
+// public:
+//     DisjointSetUnion(int n){
+//         size.resize(n,0);
+//         parent.resize(n);
+
+//         for(int i = 0;i < n;i++){
+//             parent[i] = i;
+//         }
+//     }
+    
+//     int findParent(int node){
+//         if(parent[node] == node) return node;
+//         else return parent[node] = findParent(parent[node]);
+//     }
+
+//     void unionBySize(int u, int v){
+//         int parent_u = findParent(u);
+//         int parent_v = findParent(v);
+
+//         if(parent_u == parent_v) return;
+
+//         if(size[parent_u] < size[parent_v]){
+//             parent[parent_u] = parent_v;
+//             size[parent_v] += size[parent_u];
+//         }
+//         else{
+//             parent[parent_v] = parent_u;
+//             size[parent_u] += size[parent_v];
+//         }
+//     }
+// };
+
+// class Solution {
+// public:
+//     vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
+
+//         DisjointSetUnion DSU(n);
+
+//         for(int i = 1;i < n;i++){
+//             if(nums[i] - nums[i-1] <= maxDiff){
+//                 DSU.unionBySize(i, i-1);
+//             }
+//         }
+
+//         int q_size = queries.size();
+//         vector<bool> ans(q_size, false);
+
+//         for(int i = 0;i < q_size;i++){
+//             int u = queries[i][0];
+//             int v = queries[i][1];
+
+//             if(DSU.findParent(u) == DSU.findParent(v))
+//                 ans[i] = true;
+//         }
+
+//         return ans;
+//     }
+// };
