3f6f56962d2403bb192ea2c81b84e698896952c7
Avijit Ghosh <93346632+AviG10@users.noreply.github.com>
2026-07-15 01:39:41 +0530

    Time: 984 ms (53.91%), Space: 35.5 MB (86.09%) - LeetHub

--- 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
+++ 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -1,3 +1,6 @@
+// Space Optimization
+// TC: O(n * maxEle * maxEle)
+// SC: 2 * O(maxEle * maxEle)
 class Solution {
 private:
     int MOD = 1e9 + 7;
@@ -5,32 +8,33 @@ public:
     int subsequencePairCount(vector<int>& nums) {
         int n = nums.size();
         int maxEle = *max_element(nums.begin(), nums.end());
-        
-        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(maxEle + 1, vector<int>(maxEle + 1, -1)));
+ 
+        vector<vector<int>> prev(maxEle + 1, vector<int>(maxEle + 1, -1)), cur(maxEle + 1, vector<int>(maxEle + 1, -1));
 
         for(int gcd1 = 0; gcd1 <= maxEle; gcd1++){
             for(int gcd2 = 0; gcd2 <= maxEle; gcd2++){
                 bool gcdNonZero = (gcd1 != 0 && gcd2 != 0);
                 bool gcdEqual = (gcd1 == gcd2);
 
-                dp[n][gcd1][gcd2] = (gcdNonZero && gcdEqual) ? 1 : 0;
+                prev[gcd1][gcd2] = (gcdNonZero && gcdEqual) ? 1 : 0;
             }
         }
 
         for(int i = n-1; i >= 0; i--){
             for(int gcd1 = 0; gcd1 <= maxEle; gcd1++){
                 for(int gcd2 = 0; gcd2 <= maxEle; gcd2++){
-                    int skip = dp[i + 1][gcd1][gcd2];
+                    int skip = prev[gcd1][gcd2];
 
-                    int take_seq1 = dp[i + 1][__gcd(gcd1, nums[i])][gcd2];
+                    int take_seq1 = prev[__gcd(gcd1, nums[i])][gcd2];
 
-                    int take_seq2 = dp[i + 1][gcd1][__gcd(gcd2, nums[i])];
+                    int take_seq2 = prev[gcd1][__gcd(gcd2, nums[i])];
 
-                    dp[i][gcd1][gcd2] = ((long long)skip + take_seq1 + take_seq2) % MOD;
+                    cur[gcd1][gcd2] = ((long long)skip + take_seq1 + take_seq2) % MOD;
                 }
             }
+            prev = cur;
         }
 
-        return dp[0][0][0];
+        return prev[0][0];
     }
 };
