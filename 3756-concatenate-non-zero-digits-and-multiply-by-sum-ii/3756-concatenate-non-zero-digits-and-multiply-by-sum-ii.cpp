d97b574d23acfcc971b21438938dda1429e1040d
Avijit Ghosh <93346632+AviG10@users.noreply.github.com>
2026-07-20 16:42:03 +0530

    Time: 38 ms (53.77%), Space: 155.6 MB (44.32%) - LeetHub

--- 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
+++ 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
@@ -1,63 +1,57 @@
 class Solution {
-public:
+private:
     int MOD = 1e9 + 7;
-    typedef long long ll;
-
+public:
     vector<int> sumAndMultiply(string s, vector<vector<int>>& queries) {
         int n = s.length();
 
-        vector<int> nonZeroCount(n, 0);
-        vector<ll> numberUpTo(n, 0);
-        vector<ll> digitSumUpTo(n, 0);
-        vector<ll> pow10(n + 1, 0);
-
-        pow10[0] = 1;
-        for (int i = 1; i <= n; i++)
-            pow10[i] = (pow10[i - 1] * 10) % MOD;
-
-        nonZeroCount[0] = (s[0] != '0') ? 1 : 0;
-        numberUpTo[0] = s[0] - '0';
-        digitSumUpTo[0] = s[0] - '0';
+        vector<long long> prefixSum(n, 0);
+        prefixSum[0] = s[0] - '0';
 
         for (int i = 1; i < n; i++) {
             int digit = s[i] - '0';
-            nonZeroCount[i] = nonZeroCount[i - 1] + (digit != 0 ? 1 : 0);
+            prefixSum[i] = prefixSum[i - 1] + digit;
         }
 
+        vector<int> prefixNonZeroDigit(n, 0);
+        prefixNonZeroDigit[0] = (s[0] != '0') ? 1 : 0;
+
         for (int i = 1; i < n; i++) {
             int digit = s[i] - '0';
+            prefixNonZeroDigit[i] = prefixNonZeroDigit[i - 1] + ((digit != 0) ? 1 : 0);
+        }
+
+        vector<long long> prefixNumber(n, 0);
+        prefixNumber[0] = s[0] - '0';
+
+        for (int i = 1; i < n; i++) {
+            int digit = s[i] - '0';
+
             if (digit != 0)
-                numberUpTo[i] = (numberUpTo[i - 1] * 10 + digit) % MOD;
+                prefixNumber[i] = (prefixNumber[i - 1] * 10 + digit) % MOD;
             else
-                numberUpTo[i] = numberUpTo[i - 1];
+                prefixNumber[i] = prefixNumber[i - 1];
         }
 
-        for (int i = 1; i < n; i++) {
-            digitSumUpTo[i] = digitSumUpTo[i - 1] + (s[i] - '0');
+        vector<long long> pow10(n+1,0);
+        pow10[0] = 1;
+        
+        for(int i = 1; i <= n; i++){
+            pow10[i] = (pow10[i-1] * 10) % MOD;
         }
 
-        int q = queries.size();
-        vector<int> result(q);
-
-        for (int i = 0; i < q; i++) {
+        int q_size = queries.size();
+        vector<int> result(q_size, 0);
+        for (int i = 0; i < q_size; i++) {
             int l = queries[i][0];
             int r = queries[i][1];
 
-            int startCount = (l == 0) ? 0 : nonZeroCount[l - 1];
-            ll numBefore = (l == 0) ? 0 : numberUpTo[l - 1];
+            long long sum = prefixSum[r] - ((l > 0) ? prefixSum[l - 1] : 0);
+            int k = prefixNonZeroDigit[r] - ((l > 0) ? prefixNonZeroDigit[l - 1] : 0);
+            int numberBefore = (l > 0) ? prefixNumber[l - 1] : 0;
+            long long x = (prefixNumber[r] - (numberBefore * pow10[k]) % MOD + MOD) % MOD;
 
-            int endCount = nonZeroCount[r];
-            int subStrLen = endCount - startCount;
-
-            if (subStrLen == 0) {
-                result[i] = 0;
-                continue;
-            }
-
-            ll x = (numberUpTo[r] - (numBefore * pow10[subStrLen] % MOD) + MOD) % MOD;
-            ll sum = digitSumUpTo[r] - ((l == 0) ? 0 : digitSumUpTo[l - 1]);
-
-            result[i] = (int)((x * sum) % MOD);
+            result[i] = (x * sum) % MOD;
         }
 
         return result;
