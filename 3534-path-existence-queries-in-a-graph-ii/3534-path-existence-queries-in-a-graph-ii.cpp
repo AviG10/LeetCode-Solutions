58aa66e1d09bf88dbb02aed90522cf8bb8b16c2e
Avijit Ghosh <93346632+AviG10@users.noreply.github.com>
2026-07-22 02:20:32 +0530

    Time: 365 ms (30.82%), Space: 404.4 MB (11.53%) - LeetHub

--- 3534-path-existence-queries-in-a-graph-ii/3534-path-existence-queries-in-a-graph-ii.cpp
+++ 3534-path-existence-queries-in-a-graph-ii/3534-path-existence-queries-in-a-graph-ii.cpp
@@ -1,95 +1,87 @@
 class Solution {
-public:
-    int rows;
-    int cols;
-    vector<vector<int>> ancestorTable;
-
-    int customUpperBound(vector<pair<int, int>>& arr, int target) {
+private: 
+    int LowerBound(vector<pair<int, int>> &arr, int target){
         int n = arr.size();
         int l = 0;
         int r = n-1;
-        int result = 0;
+        int result = -1;
 
-        while(l <= r) {
+        while(l <= r){
             int mid = l + (r-l)/2;
 
-            if(arr[mid].first <= target) {
+            if(arr[mid].first <= target){
                 result = mid;
-                l = mid+1;
-            } else {
-                r = mid-1;
+                l = mid + 1;
             }
+            else
+                r = mid - 1;
         }
 
         return result;
     }
-
+public:
     vector<int> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
-        //arr
-        vector<pair<int, int>> arr(n);
-        for(int i = 0; i < n; i++) {
-            arr[i] = {nums[i], i};
+        vector<pair<int, int>> arr;
+
+        for (int i = 0; i < n; i++)
+            arr.push_back({nums[i], i});
+
+        sort(arr.begin(), arr.end());
+
+        vector<int> originalIdxMapping(n, 0);
+
+        for (int i = 0; i < n; i++) {
+            int idx = arr[i].second;
+            originalIdxMapping[idx] = i;
         }
 
-        sort(begin(arr), end(arr));
-        vector<int> nodeToIdx(n);
-        for(int i = 0; i < n; i++) {
-            int node = arr[i].second;
-            nodeToIdx[node] = i;
+        int row = n;
+        int col = log2(n) + 1;
+        vector<vector<int>> ancestorTable(row, vector<int>(col, 0));
+
+        for(int node = 0; node < n; node++){
+            ancestorTable[node][0] = LowerBound(arr, arr[node].first + maxDiff);
         }
 
-        rows = n;
-        cols = log2(n)+1;
-        ancestorTable.resize(rows, vector<int>(cols, 0));
-
-        //Fill 0th column first
-        for(int node = 0; node < n; node++) { //nlogn
-            int farthestIdxOneHop = customUpperBound(arr, arr[node].first + maxDiff);
-            ancestorTable[node][0] = farthestIdxOneHop;
-        }
-
-        //Fill remaining column
-        for(int j = 1; j < cols; j++) { //logn
-            for(int node = 0; node < n; node++) { //n
-                ancestorTable[node][j] = ancestorTable[ ancestorTable[node][j-1] ][j-1];
+        for(int j = 1; j < col; j++){
+            for(int node = 0; node < n; node++){
+                ancestorTable[node][j] = ancestorTable[ancestorTable[node][j-1]][j-1];
             }
         }
 
         vector<int> result;
-        for(auto &query : queries) { //O(q)
+
+        for(auto &query : queries){
             int u = query[0];
             int v = query[1];
 
-            int a = nodeToIdx[u];
-            int b = nodeToIdx[v];
-            if(a == b) {
+            int a = originalIdxMapping[u];
+            int b = originalIdxMapping[v];
+
+            if(a == b){
                 result.push_back(0);
                 continue;
             }
-
-            if(a > b) {
+            
+            if(a > b) 
                 swap(a, b);
-            }
-
-            int curr  = a;
+            
+            int cur = a;
             int jumps = 0;
 
-            for(int j = cols-1; j >= 0; j--) { //log(n)
-                if(ancestorTable[curr][j] < b) {
-                    curr = ancestorTable[curr][j];
-                    jumps += (1 << j); //pow(2, j)
+            for(int j = col - 1; j >= 0; j--){
+                if(ancestorTable[cur][j] < b){
+                    cur = ancestorTable[cur][j];
+                    jumps += (1 << j);
                 }
             }
 
-            if(ancestorTable[curr][0] >= b) {
-                result.push_back(jumps+1);
-            } else {
+            if(ancestorTable[cur][0] >= b)
+                result.push_back(jumps + 1);
+            else 
                 result.push_back(-1);
-            }
-            
         }
 
         return result;
-
     }
 };
