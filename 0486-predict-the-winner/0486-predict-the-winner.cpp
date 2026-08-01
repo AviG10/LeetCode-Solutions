132e2a5da6f1c62f478fe6f2e14d92f63d1aea2c
Avijit Ghosh <93346632+AviG10@users.noreply.github.com>
2026-08-01 13:37:40 +0530

    Time: 83 ms (24.73%), Space: 9.7 MB (69.71%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -0,0 +1,30 @@
+class Solution {
+private:
+    int solve(int i, int j, vector<int>& nums) {
+        if (i > j)
+            return 0;
+        if (i == j)
+            return nums[i];
+
+        int take_i =
+            nums[i] + min(solve(i + 2, j, nums), solve(i + 1, j - 1, nums));
+        int take_j =
+            nums[j] + min(solve(i + 1, j - 1, nums), solve(i, j - 2, nums));
+
+        return max(take_i, take_j);
+    }
+
+public:
+    bool predictTheWinner(vector<int>& nums) {
+        int n = nums.size();
+        int totalSum = 0;
+
+        for (int i = 0; i < n; i++)
+            totalSum += nums[i];
+
+        int player1_score = solve(0, n - 1, nums);
+        int player2_score = totalSum - player1_score;
+
+        return player1_score >= player2_score;
+    }
+};
