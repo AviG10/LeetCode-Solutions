7fb9cc8ad682678c9398feb81f8978cc4335f9ea
Avijit Ghosh <93346632+AviG10@users.noreply.github.com>
2026-08-08 22:09:14 +0530

    Time: 44 ms (54.67%), Space: 52.5 MB (37.33%) - LeetHub

--- 3348-smallest-divisible-digit-product-ii/3348-smallest-divisible-digit-product-ii.cpp
+++ 3348-smallest-divisible-digit-product-ii/3348-smallest-divisible-digit-product-ii.cpp
@@ -1,75 +1,86 @@
 class Solution {
-public:
-    typedef long long ll;
+private:
+    long long primeFactorization(long long num) {
+        for (int primeFactor : {2, 3, 5, 7}) {
+            while (num % primeFactor == 0) {
+                num /= primeFactor;
+            }
+        }
+        return num;
+    }
 
-    string freeSlotsFiller(ll required, int length) {
+    void preComputeRemainingFactor(vector<long long>& remainingFactor,
+                                   string& s, int& n) {
+        for (int i = 0; i < n; i++) {
+            int digit = s[i] - '0';
+
+            if (digit == 0)
+                break;
+
+            remainingFactor[i + 1] =
+                remainingFactor[i] / gcd(remainingFactor[i], digit);
+        }
+    }
+
+    int computeZeroIdx(string& s) {
+        int n = s.length();
+        int idx = n - 1;
+        int zeroPos = s.find('0');
+
+        if (zeroPos != -1)
+            idx = zeroPos;
+
+        return idx;
+    }
+
+    string freeSlotFiller(long long required, int length) {
         string str;
 
-        for(int digit = 9; digit >= 2; digit--) {
-            while(required % digit == 0) {
-                str.push_back(digit + '0');
+        for (int digit = 9; digit >= 2; digit--) {
+            while (required % digit == 0) {
                 required /= digit;
+                str.push_back(digit + '0');
             }
         }
 
-        while(str.length() < length) {
+        while (str.length() < length)
             str.push_back('1');
-        }
 
-        reverse(begin(str), end(str));
+        reverse(str.begin(), str.end());
 
         return str;
     }
 
-    string smallestNumber(string num, long long t) {
-        int n = num.length();
+public:
+    string smallestNumber(string s, long long t) {
+        int n = s.length();
 
-        ll temp = t;
-        for(int primeFact : {2, 3, 5, 7}) {
-            while(temp % primeFact == 0) {
-                temp /= primeFact;
-            }
-        }
-
-        if(temp != 1) {
+        if (primeFactorization(t) != 1)
             return "-1";
-        }
 
-        vector<ll> remainingFactor(n+1, t);
-        for(int i = 0; i < n; i++) {
-            int digit = num[i] - '0';
+        vector<long long> remainingFactor(n + 1, t);
+        preComputeRemainingFactor(remainingFactor, s, n);
 
-            if(digit == 0) {
-                break;
-            }
+        if (remainingFactor[n] == 1)
+            return s;
 
-            remainingFactor[i+1] = remainingFactor[i]/gcd(remainingFactor[i], (ll)digit);
-        }
+        int zeroIdx = computeZeroIdx(s);
 
-        if(remainingFactor[n] == 1) { 
-            return num;
-        }
+        for (int i = zeroIdx; i >= 0; i--) {
+            long long required = remainingFactor[i];
+            int freeSlot = n - i - 1;
 
-        int zeroPos = num.find('0');
-        int zeroIdx = n-1;
-        if(zeroPos != -1) {
-            zeroIdx = zeroPos;
-        }
+            for (int digit = (s[i] - '0')+1; digit <= 9; digit++) {
+                long long furtherRequired =
+                    required / gcd(required, (long long)digit);
+                string requiredNumber =
+                    freeSlotFiller(furtherRequired, freeSlot);
 
-        for(int i = zeroIdx; i >= 0; i--) {
-            ll required = remainingFactor[i];
-            int freeSlots = n - 1 - i;
-
-            for(int digit = (num[i] - '0')+1; digit <= 9; digit++) {
-                ll furtherRequired = required / gcd(required, digit);
-                string requiredNumber = freeSlotsFiller(furtherRequired, freeSlots);
-
-                if(requiredNumber.length() == freeSlots) {
-                    return num.substr(0, i) + char(digit + '0') + requiredNumber;
-                }
+                if (requiredNumber.length() == freeSlot)
+                    return s.substr(0, i) + char(digit + '0') + requiredNumber;
             }
         }
 
-        return freeSlotsFiller(t, n+1); 
+        return freeSlotFiller(t, n + 1);
     }
 };
