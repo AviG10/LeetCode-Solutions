15d73dbcd123c460e965262837c329efe357bf28
Avijit Ghosh <93346632+AviG10@users.noreply.github.com>
2026-08-09 20:26:23 +0530

    Time: 18 ms (30.54%), Space: 16.9 MB (20.74%) - LeetHub

---
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+private:
+    int solve(int player, int i, int M, int& n, vector<int>& piles, vector<vector<vector<int>>> &dp) {
+        if (i >= n)
+            return 0;
+
+        if(dp[player][i][M] != -1) 
+            return dp[player][i][M];
+        
+        int ans = (player == 0) ? INT_MIN : INT_MAX;
+        int stones = 0;
+
+        for (int x = 1; x <= min(2 * M, n - i); x++) {
+            stones += piles[i + x - 1];
+
+            if (player == 0)
+                ans = max(ans, stones + solve(1, i + x, max(M, x), n, piles, dp));
+            else
+                ans = min(ans, solve(0, i + x, max(M, x), n, piles, dp));
+        }
+
+        return dp[player][i][M] = ans;
+    }
+
+public:
+    int stoneGameII(vector<int>& piles) {
+        int n = piles.size();
+        vector<vector<vector<int>>> dp(2, vector<vector<int>>(n+1, vector<int>(n+1, -1)));
+
+        return solve(0, 0, 1, n, piles, dp);
+    }
+};
+
+// 0 - Alice
+// 1 - Bob
