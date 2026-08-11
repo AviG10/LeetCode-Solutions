bc62bfac4d5cf17a55f62b36f555975f86565f2a
Avijit Ghosh <93346632+AviG10@users.noreply.github.com>
2026-08-11 09:35:20 +0530

    Time: 0 ms (100%), Space: 23 MB (29.32%) - LeetHub

---
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int missingInteger(vector<int>& nums) {
+        int n = nums.size();
+        int prefixSum = nums[0];
+        bool prefix = true;
+        unordered_map<int, int> mp;
+
+        mp[nums[0]]++;
+
+        for(int i = 1;i < n;i++){
+            if(prefix){
+                if(nums[i] == nums[i-1] + 1)
+                    prefixSum += nums[i];
+                else 
+                    prefix = false;
+            }
+
+            mp[nums[i]]++;
+        }
+ 
+        while(mp[prefixSum] > 0)
+            prefixSum++;
+
+        return prefixSum;
+    }
+};
