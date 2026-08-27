331e7370601ade45471c3493c417dc7c4ddde77b
Avijit Ghosh <93346632+AviG10@users.noreply.github.com>
2026-08-28 02:14:57 +0530

    Time: 0 ms (100%), Space: 10.3 MB (5.14%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,27 +1,24 @@
 class Solution {
-private:
-    bool f(int i, int j, int& m, int& n, string& s, string& t,
-           vector<vector<int>>& dp) {
-        if (i == m)
-            return true;
-        if (j == n)
-            return false;
-
-        if (dp[i][j] != -1)
-            return dp[i][j];
-
-        if (s[i] == t[j])
-            return dp[i][j] = f(i + 1, j + 1, m, n, s, t, dp) ||
-                              f(i, j + 1, m, n, s, t, dp);
-        else
-            return dp[i][j] = f(i, j + 1, m, n, s, t, dp);
-    }
-
 public:
     bool isSubsequence(string s, string t) {
         int m = s.length(), n = t.length();
-        vector<vector<int>> dp(m, vector<int>(n, -1));
+        vector<vector<int>> dp(m + 1, vector<int>(n + 2, -1));
 
-        return f(0, 0, m, n, s, t, dp);
+        for (int j = 0; j < n; j++)
+            dp[m][j] = true;
+
+        for (int i = 0; i < m; i++)
+            dp[i][n] = false;
+
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (s[i] == t[j])
+                    dp[i][j] = dp[i + 1][j + 1] || dp[i][j + 1];
+                else
+                    dp[i][j] = dp[i][j + 1];
+            }
+        }
+
+        return dp[0][0];
     }
 };
