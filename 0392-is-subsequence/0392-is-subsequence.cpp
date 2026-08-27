cea6cfdb3bc742ed47c4169141bd21fd92e83e6e
Avijit Ghosh <93346632+AviG10@users.noreply.github.com>
2026-08-28 02:40:41 +0530

    Time: 5 ms (2.11%), Space: 14.4 MB (5.14%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,20 +1,26 @@
 class Solution {
 private:
-    int findNext(int j, char c, string &t){
+    int findNext(int j, char ch, string &t, vector<vector<int>> &dp){
         if(j == t.length())
             return -1;
         
-        if(c == t[j]) 
-            return j;
-        
-        return findNext(j+1, c, t);
+        if(dp[j][ch - 'a'] != -2) 
+            return dp[j][ch - 'q'];
+
+        if(ch == t[j]) 
+            return dp[j][ch - 'a'] = j;
+        else 
+            return dp[j][ch - 'a'] = findNext(j+1, ch, t, dp);
     }
 public:
     bool isSubsequence(string s, string t) {
         int cur_j = 0;
+        int n = t.length();
+
+        vector<vector<int>> dp(n+1, vector<int>(26, -2));
 
         for(char &ch : s){
-            int next_idx = findNext(cur_j, ch, t);
+            int next_idx = findNext(cur_j, ch, t, dp);
             
             if(next_idx == -1) 
                 return false;
