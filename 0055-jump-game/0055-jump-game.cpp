6429d992a1f124cd4a6e5920bdfb9f2cf73d060c
Avijit Ghosh <93346632+AviG10@users.noreply.github.com>
2026-08-28 23:22:28 +0530

    Time: 646 ms (5.51%), Space: 56 MB (5.98%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,21 +1,23 @@
 class Solution {
 private:
-    int f(int i, int &n, vector<int>& nums, vector<int> &dp){
-        if(i == n-1) return true;
+    int f(int i, int& n, vector<int>& nums, vector<int>& dp) {
+        if (i == n - 1)
+            return true;
 
-        if(dp[i] != -1) 
+        if (dp[i] != -1)
             return dp[i];
 
         bool isPossible = false;
-        for(int jump = 1; jump <= nums[i]; jump++){
-            if(i + jump < n) 
-                isPossible = isPossible || f(i+jump, n, nums, dp);
-            if(isPossible)
+        for (int jump = 1; jump <= nums[i]; jump++) {
+            if (i + jump < n)
+                isPossible = isPossible || f(i + jump, n, nums, dp);
+            if (isPossible)
                 break;
         }
 
         return dp[i] = isPossible;
     }
+
 public:
     bool canJump(vector<int>& nums) {
         int n = nums.size();
