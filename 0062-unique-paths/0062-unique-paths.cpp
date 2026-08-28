20e91abd44ce9c916be98c8f625d684808aad847
Avijit Ghosh <93346632+AviG10@users.noreply.github.com>
2026-08-28 20:30:57 +0530

    Time: 0 ms (100%), Space: 9.4 MB (30.94%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,29 +1,32 @@
 class Solution {
 private:
-    bool isValid(int i, int j, int &m, int &n){
+    bool isValid(int i, int j, int& m, int& n) {
         return i >= 0 && i < m && j >= 0 && j < n;
     }
 
-    int f(int i, int j, int &m, int &n, vector<vector<int>> &dp){
-        if(i == m-1 && j == n-1) return 1;
-        
-        if(dp[i][j] != -1) 
-            return dp[i][j];
-
-        int down = 0;
-        if(isValid(i+1, j, m, n))
-            down = f(i+1,j, m, n, dp);
-        
-        int right = 0;
-        if(isValid(i, j+1, m, n))
-            right = f(i, j+1,m, n, dp);
-
-        return dp[i][j] = down + right;
-    }
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m+1, vector<int>(n, -1));
+        vector<vector<int>> dp(m + 1, vector<int>(n, -1));
 
-        return f(0, 0, m, n, dp);
+        dp[m - 1][n - 1] = 1;
+
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (i == m - 1 && j == n - 1)
+                    continue;
+
+                int down = 0;
+                if (isValid(i + 1, j, m, n))
+                    down = dp[i + 1][j];
+
+                int right = 0;
+                if (isValid(i, j + 1, m, n))
+                    right = dp[i][j + 1];
+
+                dp[i][j] = down + right;
+            }
+        }
+
+        return dp[0][0];
     }
 };
