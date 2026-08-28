c44559cbd63661e54b713abd152a7691aa77c5e7
Avijit Ghosh <93346632+AviG10@users.noreply.github.com>
2026-08-28 23:21:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,24 @@
+class Solution {
+private:
+    int f(int i, int &n, vector<int>& nums, vector<int> &dp){
+        if(i == n-1) return true;
+
+        if(dp[i] != -1) 
+            return dp[i];
+
+        bool isPossible = false;
+        for(int jump = 1; jump <= nums[i]; jump++){
+            if(i + jump < n) 
+                isPossible = isPossible || f(i+jump, n, nums, dp);
+        }
+
+        return dp[i] = isPossible;
+    }
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, -1);
+
+        return f(0, n, nums, dp);
+    }
+};
