2e206470efa9de4cae8ad4c5b93a0f3783f9c14d
Avijit Ghosh <93346632+AviG10@users.noreply.github.com>
2026-09-02 02:59:12 +0530

    Time: 86 ms (17.47%), Space: 9.7 MB (53.62%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,20 +1,17 @@
 class Solution {
 private:
-    int solve(int i, int j, vector<int>& nums, vector<vector<int>>& dp) {
+    int solve(int i, int j, vector<int>& nums) {
         if (i > j)
             return 0;
         if (i == j)
             return nums[i];
 
-        if (dp[i][j] != -1)
-            return dp[i][j];
+        int take_i =
+            nums[i] + min(solve(i + 1, j - 1, nums), solve(i + 2, j, nums));
+        int take_j =
+            nums[j] + min(solve(i + 1, j - 1, nums), solve(i, j - 2, nums));
 
-        int take_i = nums[i] + min(solve(i + 2, j, nums, dp),
-                                   solve(i + 1, j - 1, nums, dp));
-        int take_j = nums[j] + min(solve(i + 1, j - 1, nums, dp),
-                                   solve(i, j - 2, nums, dp));
-
-        return dp[i][j] = max(take_i, take_j);
+        return max(take_i, take_j);
     }
 
 public:
@@ -25,8 +22,7 @@ public:
         for (int i = 0; i < n; i++)
             totalSum += nums[i];
 
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-        int player1_score = solve(0, n - 1, nums, dp);
+        int player1_score = solve(0, n - 1, nums);
         int player2_score = totalSum - player1_score;
 
         return player1_score >= player2_score;
