8d163ef3d52e141f7b0ab9faff334338c641cce2
Avijit Ghosh <93346632+AviG10@users.noreply.github.com>
2026-09-10 21:08:09 +0530

    Time: 53 ms (19.09%), Space: 75.5 MB (40.64%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,11 +10,11 @@
  */
 class Solution {
 private:
-    ListNode* findMid(ListNode* head){
+    ListNode* findMid(ListNode* head) {
         ListNode* slow = head;
         ListNode* fast = head->next;
 
-        while(fast != NULL && fast->next != NULL){
+        while (fast != NULL && fast->next != NULL) {
             fast = fast->next->next;
             slow = slow->next;
         }
@@ -22,38 +22,38 @@ private:
         return slow;
     }
 
-    ListNode* mergeTwoSortedList(ListNode* head1, ListNode* head2){
+    ListNode* mergeTwoSortedList(ListNode* head1, ListNode* head2) {
         ListNode* dummy = new ListNode(-1);
         ListNode* temp = dummy;
 
         ListNode* temp1 = head1;
         ListNode* temp2 = head2;
 
-        while(temp1 != NULL && temp2 != NULL){
-            if(temp1->val < temp2->val){
+        while (temp1 != NULL && temp2 != NULL) {
+            if (temp1->val < temp2->val) {
                 temp->next = temp1;
                 temp1 = temp1->next;
-            }
-            else{
+            } else {
                 temp->next = temp2;
                 temp2 = temp2->next;
             }
-            
+
             temp = temp->next;
         }
 
-        if(temp1 != NULL)
+        if (temp1 != NULL)
             temp->next = temp1;
         else
             temp->next = temp2;
-        
+
         return dummy->next;
     }
+
 public:
     ListNode* sortList(ListNode* head) {
-        if(head == NULL || head->next == NULL)
+        if (head == NULL || head->next == NULL)
             return head;
-        
+
         ListNode* midNode = findMid(head);
 
         ListNode* right = midNode->next;
@@ -63,7 +63,7 @@ public:
 
         ListNode* leftSortedList = sortList(left);
         ListNode* rightSortedList = sortList(right);
-        
+
         return mergeTwoSortedList(leftSortedList, rightSortedList);
     }
 };
