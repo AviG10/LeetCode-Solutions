48cc21839953b560a36e43235eae7e99e7edbb63
Avijit Ghosh <93346632+AviG10@users.noreply.github.com>
2026-09-22 02:20:55 +0530

    Time: 507 ms (14.28%), Space: 328.1 MB (11.61%) - LeetHub

--- 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.cpp
+++ 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.cpp
@@ -1,33 +1,4 @@
 class Solution {
-private:
-    // Subarray starting with index i, and Prev Product remainder prevProd
-    long long solve(int i, int prevProd, int& x, int& k, int& n,
-                    vector<int>& nums, vector<vector<long long>>& dp) {
-        if (i >= n)
-            return 0;
-
-        if (dp[i][prevProd] != -1)
-            return dp[i][prevProd];
-
-        // Not Take
-        long long notTake = 0;
-
-        if (prevProd == k)
-            notTake = solve(i + 1, prevProd, x, k, n, nums, dp);
-
-        // Take
-        long long curProd;
-        if (prevProd == k)
-            curProd = nums[i];
-        else
-            curProd = (prevProd * nums[i]) % k;
-
-        long long take =
-            (curProd == x) + solve(i + 1, curProd, x, k, n, nums, dp);
-
-        return dp[i][prevProd] = (notTake + take);
-    }
-
 public:
     vector<long long> resultArray(vector<int>& nums, int k) {
         int n = nums.size();
@@ -37,8 +8,31 @@ public:
             nums[i] = nums[i] % k;
 
         for (int x = 0; x <= k - 1; x++) {
-            vector<vector<long long>> dp(n, vector<long long>(k + 1, -1));
-            result[x] = solve(0, k, x, k, n, nums, dp);
+            vector<vector<long long>> dp(n + 1, vector<long long>(k + 1, 0));
+
+            for (int i = n - 1; i >= 0; i--) {
+                
+                for (int prevProd = 0; prevProd <= k; prevProd++) {
+                    // Not Take
+                    long long notTake = 0;
+
+                    if (prevProd == k)
+                        notTake = dp[i + 1][prevProd];
+
+                    // Take
+                    long long curProd;
+                    if (prevProd == k)
+                        curProd = nums[i];
+                    else
+                        curProd = (prevProd * nums[i]) % k;
+
+                    long long take = (curProd == x) + dp[i + 1][curProd];
+
+                    dp[i][prevProd] = (notTake + take);
+                }
+            }
+
+            result[x] = dp[0][k];
         }
 
         return result;
